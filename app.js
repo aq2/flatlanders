478b0cb2fc588c9f6afa7c32400c437ff23b91ad
angelo <aq211@exeter.ac.uk>
2017-07-15 11:44:30 +0100

    initial commitial

---
+++ app.js
@@ -0,0 +1,39 @@
+(function() {
+  var app = angular.module('store', [])
+
+  app.controller('StoreController', function() {
+    this.products = gems
+  })
+
+  const ginge = 5
+  var teeth = 'gorilla'
+  var lemon = teeth + ginge
+  // teeth = lemon
+
+  // fixme this isn't working
+
+  // todo this needs to be done
+
+  // here is more stuff
+
+  // aq oh noes
+
+  var gems = [
+    {
+      name: 'Docdecahedron',
+      price: 2.95,
+      description: '. .df .',
+      canPurchase: true,
+      soldOut: false
+    },
+    {
+      name: 'Pentagonal Gem',
+      price: 5.95,
+      description: ' ..... ',
+      canPurchase: true,
+      soldOut: false
+    }
+  ]
+
+
+})()
